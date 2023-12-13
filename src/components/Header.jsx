a2ca2f78006733716b78da9a72415be6a2b6cb65
Ricardo González <ricardoisrael.gonzalez@gmail.com>
2023-12-12 20:23:23 -0700

    Agrega tareas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,52 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux"
+import { logout, reset } from '../features/auth/authSlice'
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const {user} = useSelector((state)=> state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+    }
+
   return (
     <header className='header'>
         <div className="logo">
             <Link to='/'>App Tareas</Link>
         </div>
         <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUser /> Register
-                </Link>
-            </li>
+            {/*SI HAY UN USUARIO LOGIN MUESTRA EL BOTÓN DE LOGOUT. SI NO MUESTRA LOS BOTONES LOGIN Y REGISTER*/}
+            
+            {user ? 
+            (
+                <>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignOutAlt /> Logout
+                    </button>
+                </>
+            ) 
+            : 
+            (
+                <>
+                    <li>
+                    <Link to='/login'>
+                        <FaSignInAlt /> Login
+                    </Link>
+                    </li>
+                    <li>
+                        <Link to='/register'>
+                            <FaUser /> Register
+                        </Link>
+                    </li>
+                </>
+            )}
+            
         </ul>
     </header>
   )
