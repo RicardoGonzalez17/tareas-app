6080888b2055f9894f5cb08490b38348ef3b6e7c
Ricardo González <ricardoisrael.gonzalez@gmail.com>
2023-12-14 19:26:53 -0700

    Front terminado

--- src/features/tareas/tareaSlice.js
+++ src/features/tareas/tareaSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import tareaService from './tareaService'
 
 const initialState = {
-    tareas: [],
+    mistareas: [],
     isError: false,
     isSuccess: false,
     isLoading: false,
@@ -14,6 +14,28 @@ export const crearTarea = createAsyncThunk('tareas/crear', async (tareaData, thu
     try {
         const token = thunkAPI.getState().auth.user.token
         return await tareaService.crearTarea(tareaData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+}) 
+
+//Obtener las tareas del usuario
+export const getTareas = createAsyncThunk('tareas/getTareas', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await tareaService.getTareas(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+// delete tarea
+export const deleteTarea = createAsyncThunk('tareas/delete', async (id, thunkAPI)=> {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await tareaService.deleteTarea(id, token)
     } catch (error) {
         const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
         return thunkAPI.rejectWithValue(message)
@@ -34,9 +56,39 @@ export const tareaSlices = createSlice({
             .addCase(crearTarea.fulfilled, (state, action)=>{
                 state.isLoading = false
                 state.isSuccess = true
-                state.tareas.push(action.payload)
+                state.mistareas.push(action.payload)
             })
             .addCase(crearTarea.rejected, (state, action)=>{
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            
+
+            .addCase(getTareas.pending, (state)=>{
+                state.isLoading = true
+            })
+            .addCase(getTareas.fulfilled, (state, action)=>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.mistareas = action.payload
+            })
+            .addCase(getTareas.rejected, (state, action)=>{
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+
+            .addCase(deleteTarea.pending, (state)=>{
+                state.isLoading = true
+            })
+            .addCase(deleteTarea.fulfilled, (state, action)=>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.mistareas = state.mistareas.filter((tarea)=> tarea._id !== action.payload.id)
+            })
+            .addCase(deleteTarea.rejected, (state, action)=>{
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
